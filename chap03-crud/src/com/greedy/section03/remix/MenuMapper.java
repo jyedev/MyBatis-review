cfe487fa7039d75343a8819f70a2283ed12521e0
주영은 <ynyndev@gmail.com>
2022-03-16 17:43:55 +0900

    crud-remix 리뷰

---
+++ chap03-crud/src/com/greedy/section03/remix/MenuMapper.java
@@ -0,0 +1,17 @@
+package com.greedy.section03.remix;
+
+import java.util.List;
+
+public interface MenuMapper {
+
+	List<MenuDTO> selectAllMenu();
+
+	MenuDTO selectMenuByCode(int code);
+
+	int insertMenu(MenuDTO menu);
+
+	int modifyMenu(MenuDTO menu);
+
+	int deleteMenu(int code);
+
+}
